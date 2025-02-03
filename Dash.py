78718df1f4799b549b0001ba8cce99609e88ef1d
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-02-03 16:03:46 -0300

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -58,10 +58,10 @@ pagamento = df_filter.groupby("Payment")[["Quantity","Unit price", "cogs","Tax 5
 pagamento = pagamento.sort_values("Total")
 
 grafico_3 = px.pie(pagamento, values="Total", names="Payment", hole=0.5, title="DISTRIBUIÇÃO POR FORMA DE PAGAMENTO %", 
-color="Payment", color_discrete_map={"Cash":"#FFABAB", "Ewallet":"#0068C9", "Credit card":"#83C9FF"})
+color="Payment", color_discrete_map={"Cash":"#FF2B2B", "Ewallet":"#0068C9", "Credit card":"#83C9FF"})
 col3.plotly_chart(grafico_3, use_container_width=True)
 
-grafico_4 = px.bar(pagamento, x="Payment", y="Total", title="FATURAMENTO POR FORMA DE PAGAMENTO", color="Payment", text_auto=True)
+grafico_4 = px.bar(pagamento, x="Payment", y="Total", title="FATURAMENTO POR FORMA DE PAGAMENTO", color="Payment", color_discrete_map={"Cash":"#FF2B2B", "Ewallet":"#0068C9", "Credit card":"#83C9FF"}, text_auto=True)
 col4.plotly_chart(grafico_4, use_container_width=True)
 
 st.divider()
